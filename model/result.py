670b23f8855b2530f349ad1f72a3834a239412a6
unknown <sibjoma@skku.edu>
2023-02-12 18:49:36 +0900

    scatter result add

--- model/result.py
+++ model/result.py
@@ -1,13 +1,68 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
 from sklearn.metrics import r2_score, mean_absolute_error
 
 class SingleAnalyzer:
-    def __init__(self, cluster_model, whole_coord):
+    def __init__(self, model_name, cluster_model, whole_coord, input_data: pd.DataFrame, label_data: pd.Series, pred_data: pd.Series, train_test_id):
+        self.model_name = model_name
         self.cluster_model = cluster_model
         self.whole_coord = whole_coord
+        self.input_data = input_data
+        self.label_data = label_data
+        self.pred_data = pred_data
+        self.train_test_id = train_test_id
         self.whole_clsuter = cluster_model.predict(whole_coord)
+        
+        self.coord_info = self._compute_coord_info()
+        self.cluster_info = self._compute_cluster_info()
+
+    def _compute_coord_info(self):
+        monitor_coor = self.input_data[['cmaq_x', 'cmaq_y', 'cmaq_id']]
+        coord_info = monitor_coor.drop_duplicates(subset=['cmaq_id']).reset_index(drop=True)
+        all_pm_mean, all_r2, all_corr, all_pm_std, all_mae = [], [], [], [], []
+        for unique_coor_id in coord_info.index:
+            coor_id = coord_info.loc[unique_coor_id, 'cmaq_id']
+            coor_data = self.input_data.loc[self.input_data['cmaq_id']==coor_id]
+            if len(coor_data) < 5:
+                coord_info = coord_info[coord_info["cmaq_id"] != coor_id]
+                continue
+            coor_label = self.label_data[self.input_data['cmaq_id']==coor_id]
+            coor_pred = self.pred_data[self.input_data['cmaq_id']==coor_id]
+            pm_mean = coor_label.mean()
+            pm_std = coor_label.std()
+            r2_val = r2_score(coor_label, coor_pred)
+            corr_val = stats.pearsonr(coor_label, coor_pred).statistic
+            mae_val = mean_absolute_error(coor_label, coor_pred)
+            all_pm_mean.append(pm_mean)
+            all_pm_std.append(pm_std)
+            all_r2.append(r2_val)
+            all_corr.append(corr_val)
+            all_mae.append(mae_val)
+        coord_info['pm25_mean'] = all_pm_mean
+        coord_info['pm25_std'] = all_pm_std
+        coord_info['pm25_r2'] = all_r2
+        coord_info['pm25_corr'] = all_corr
+        coord_info['pm25_mae'] = all_mae
+        return coord_info
+
+    def _compute_cluster_info(self):
+        cluster_info = {}
+        for cluster_id in self.train_test_id.keys():
+            cluster_test_index = self.train_test_id[cluster_id]['test_cluster']
+            cluster_label = self.label_data[cluster_test_index]
+            cluster_pred = self.pred_data[cluster_test_index]
+            cluster_r2 = round(r2_score(cluster_label, cluster_pred), 4)
+            cluster_mae = round(mean_absolute_error(cluster_label, cluster_pred), 4)
+            cluster_info[cluster_id] = {
+                'r2':cluster_r2, 'mae':cluster_mae
+            }
+        cluster_info["whole"] = {
+            "r2": round(r2_score(self.label_data, self.pred_data), 4),
+            "mae": round(mean_absolute_error(self.label_data, self.pred_data), 4),
+        }
+        return cluster_info
 
     def plot_whole_cluster(self, save, alpha):
         plt.figure(figsize=(15,8))
@@ -18,81 +73,50 @@ class SingleAnalyzer:
         if save:
             plt.savefig("figures/USA_whole_cluster")
         
-    def plot_pm_vals(self, monitoring_data: pd.DataFrame):
-        monitor_coor_pm = monitoring_data[['cmaq_x', 'cmaq_y', 'cmaq_id', 'pm25_value']]
-        unique_coords = monitor_coor_pm.drop_duplicates(subset=['cmaq_id'])
-        all_pm_mean = []
-        for unique_coor_id in unique_coords.index:
-            coor_id = unique_coords.loc[unique_coor_id, 'cmaq_id']
-            coor_data = monitor_coor_pm.loc[monitor_coor_pm['cmaq_id']==coor_id]
-            pm_mean = coor_data['pm25_value'].mean()
-            all_pm_mean.append(pm_mean)
-        unique_coords['pm25_value'] = all_pm_mean
-        min_val = np.percentile(unique_coords['pm25_value'], 5)
-        max_val = np.percentile(unique_coords['pm25_value'], 95)
-
+    def plot_pm_vals(self):
+        min_val = np.percentile(self.coord_info['pm25_mean'], 5)
+        max_val = np.percentile(self.coord_info['pm25_mean'], 95)
         self.plot_whole_cluster(False, 0.1)
-        plt.scatter(unique_coords['cmaq_x'], unique_coords['cmaq_y'], s=3, 
-        c=unique_coords['pm25_value'], cmap='turbo', vmin=min_val, vmax=max_val)
+        plt.scatter(self.coord_info['cmaq_x'], self.coord_info['cmaq_y'], s=3, 
+        c=self.coord_info['pm25_mean'], cmap='turbo', vmin=min_val, vmax=max_val)
         plt.colorbar()
         plt.savefig("figures/PM25_whole_average_values")
         plt.cla()
         plt.clf()
 
-    def plot_errors(self, train_test_id: dict, monitoring_data: pd.DataFrame, model_name: str):
-        all_pred_data = np.load(f"result/{model_name}.npz")
-        all_coord_values = []
-        for cluster_id in train_test_id.keys():
-            cluster_test_index = train_test_id[cluster_id]['test_cluster']
-            cluster_label =  monitoring_data.loc[cluster_test_index, "pm25_value"]
-            cluster_pred = all_pred_data[f"cluster{cluster_id}"]
-            cluster_r2 = round(r2_score(cluster_label, cluster_pred), 4)
-            cluster_mae = round(mean_absolute_error(cluster_label, cluster_pred), 4)
-            print(f"cluster{cluster_id}")
-            print(f"R-squared: {cluster_r2}")
-            print(f"MAE: {cluster_mae}")
-            print('-'*70)
-
-            cluster_xy_val = monitoring_data.loc[cluster_test_index, ['cmaq_x', 'cmaq_y', 'cmaq_id', 'pm25_value']]
-            unique_coords = cluster_xy_val.drop_duplicates(subset=['cmaq_id'])
-            coords_values = unique_coords.copy()
-            all_pm_mean, all_r2 = [], []
-            for unique_coor_id in unique_coords.index:
-                coor_id = unique_coords.loc[unique_coor_id, 'cmaq_id']
-                coor_data = cluster_xy_val.loc[cluster_xy_val['cmaq_id']==coor_id]
-                coor_pred = cluster_pred[cluster_xy_val['cmaq_id']==coor_id]
-                pm_mean = coor_data['pm25_value'].mean()
-                r2_val = r2_score(coor_data['pm25_value'], coor_pred)
-                all_pm_mean.append(pm_mean)
-                all_r2.append(r2_val)
-            coords_values['pm25_value'] = all_pm_mean
-            coords_values['r2_value'] = all_r2
-            all_coord_values.append(coords_values.copy())
-        all_coord_values = pd.concat(all_coord_values)
-
-        min_val = np.percentile(all_coord_values['pm25_value'], 5)
-        max_val = np.percentile(all_coord_values['pm25_value'], 95)
+    def plot_accuracy_map(self):
         self.plot_whole_cluster(False, 0.1)
-        plt.scatter(all_coord_values['cmaq_x'], all_coord_values['cmaq_y'], s=3, 
-        c=all_coord_values['pm25_value'], cmap='turbo', vmin=min_val, vmax=max_val)
+        plt.scatter(self.coord_info['cmaq_x'], self.coord_info['cmaq_y'], s=3, 
+        c=self.coord_info['pm25_r2'], cmap='rainbow', vmin=0, vmax=1)
+        plt.title(self.model_name)
         plt.colorbar()
-        plt.savefig("figures/PM25_test_average_values")
+        plt.savefig(f"figures/{self.model_name}_R2_map")
         plt.cla()
         plt.clf()
 
-        self.plot_whole_cluster(False, 0.1)
-        plt.scatter(all_coord_values['cmaq_x'], all_coord_values['cmaq_y'], s=3, 
-        c=all_coord_values['r2_value'], cmap='rainbow', vmin=0, vmax=1)
-        plt.colorbar()
-        plt.savefig(f"figures/{model_name}_R2_map")
+    def hist_accuracy(self):
+        high_r2 = self.coord_info['pm25_r2'][self.coord_info['pm25_r2']>0]
+        fig, axs = plt.subplots(2,1)
+        axs[0].hist(self.coord_info['pm25_r2'], bins=30)
+        axs[1].hist(high_r2, bins=30)
+        fig.suptitle(self.model_name)
+        plt.savefig(f"figures/{self.model_name}_R2_histogram")
         plt.cla()
         plt.clf()
 
-        high_r2 = all_coord_values['r2_value'][all_coord_values['r2_value']>0]
-        plt.subplot(1,2,1)
-        plt.hist(all_coord_values['r2_value'], bins=30)
-        plt.subplot(1,2,2)
-        plt.hist(high_r2, bins=30)
-        plt.savefig(f"figures/{model_name}_R2_histogram")
+    def scatter_std_accuracy(self):
+        fig, axs = plt.subplots(2,2)
+        fig.set_figheight(9)
+        fig.set_figwidth(9)
+        axs[0,0].scatter(self.coord_info['pm25_std'], self.coord_info['pm25_r2'], s=3)
+        axs[0,0].set_xlabel("std")
+        axs[0,0].set_ylabel("R-squared")
+        axs[0,1].scatter(self.coord_info['pm25_std'], self.coord_info['pm25_mae'], s=3)
+        axs[0,1].set_xlabel("std")
+        axs[0,1].set_ylabel("mae")
+        axs[1,0].scatter(self.coord_info['pm25_std'], self.coord_info['pm25_corr'], s=3)
+        axs[1,0].set_xlabel("std")
+        axs[1,0].set_ylabel("correlation")
+        plt.savefig(f"figures/{self.model_name}_std_R2")
         plt.cla()
         plt.clf()
--- test_modules/result_test.py
+++ test_modules/result_test.py
@@ -10,24 +10,26 @@ def _get_clusters(input_dt: pd.DataFrame, label_dt: pd.Series):
     _, train_test_data_id = single_grid.split_train_test(input_dt, whole_cluster)
     return train_test_data_id, single_grid.cluster_model
 
-def _get_labels(label_dt: pd.DataFrame, train_test_data_id: dict):
-    all_labels = []
+def _get_input_label(input_dt: pd.DataFrame, label_dt: pd.DataFrame, train_test_data_id: dict):
+    all_inputs, all_labels = [], []
     for cluster_id in train_test_data_id.keys():
         cluster_test_index = train_test_data_id[cluster_id]['test_cluster']
         all_labels.append(label_dt[cluster_test_index])
-    all_labels = np.hstack(all_labels)
-    return all_labels
+        all_inputs.append(input_dt.loc[cluster_test_index])
+    all_labels = pd.concat(all_labels)
+    all_inputs = pd.concat(all_inputs)
+    return all_inputs, all_labels
 
-def _get_results(model_name: str):
+def _get_results(model_name: str, index):
     result_data = np.load(f"result/{model_name}.npz")
     all_pred_data = []
     for cluster_id in range(10):
         all_pred_data.append(result_data[f"cluster{cluster_id}"])
-    all_pred_data = np.hstack(all_pred_data)
+    all_pred_data = pd.Series(np.hstack(all_pred_data), index=index)
     return all_pred_data
 
 if __name__=='__main__':
-    model_name = "RF"
+    model_name = "FNN"
     tag_names = ['day', 'month', 'cmaq_x', 'cmaq_y', 'cmaq_id', 'rid', 'elev', 'forest_cover', 'pd', 'local', 'limi', 'high', 'is', 
     'nldas_pevapsfc','nldas_pressfc', 'nldas_cape', 'nldas_ugrd10m', 'nldas_vgrd10m', 'nldas_tmp2m', 'nldas_rh2m', 'nldas_dlwrfsfc', 
     'nldas_dswrfsfc', 'nldas_pcpsfc', 'nldas_fpcsfc', 'gc_aod', 'aod_value', 'emissi11_pm25', 'pm25_value_k', 'pm25_value']
@@ -38,14 +40,12 @@ if __name__=='__main__':
     input_dt = monitoring_whole_data.drop(columns=["pm25_value"])
     label_dt = monitoring_whole_data["pm25_value"]
     train_test_data_id, cluster_model = _get_clusters(input_dt, label_dt)
-    all_label = _get_labels(label_dt, train_test_data_id)
-    all_pred = _get_results(model_name)
-    r2_val = round(r2_score(all_label, all_pred), 4)
-    mae_val = round(mean_absolute_error(all_label, all_pred), 4)
+    all_input, all_label = _get_input_label(input_dt, label_dt, train_test_data_id)
+    all_pred = _get_results(model_name, all_label.index)
 
-    single_analyzer = SingleAnalyzer(cluster_model, whole_coord)
+    single_analyzer = SingleAnalyzer(model_name, cluster_model, whole_coord, all_input, all_label, all_pred, train_test_data_id)
     single_analyzer.plot_whole_cluster(True, 1)
-    single_analyzer.plot_pm_vals(monitoring_whole_data)
-    single_analyzer.plot_errors(train_test_data_id, monitoring_whole_data, model_name)
-    print(f"Whole MAE: {mae_val}")
-    print(f"Whole R-squared: {r2_val}")
+    single_analyzer.plot_pm_vals()
+    single_analyzer.plot_accuracy_map()
+    single_analyzer.hist_accuracy()
+    single_analyzer.scatter_std_accuracy()
